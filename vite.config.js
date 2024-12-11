36c916dac69b6fbf38ef8c0d56840ed37d28ec20
Sergey_Begiyan <124567856+serbegiyan@users.noreply.github.com>
2024-12-11 16:44:31 +0300

    lar sh lab translation.php

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,14 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/addToBasket.js',
+                'resources/js/addToBasketList.js',
+                'resources/js/basket_edit.js',
+                'resources/js/main.js',
+                'resources/js/modalka.js',
+                'resources/js/take_number.js',
+                'resources/js/take_number_list.js',
                 'resources/js/user_number.js',
-                'resources/js/take_number_list.js'
             ],
             refresh: true,
         }),
