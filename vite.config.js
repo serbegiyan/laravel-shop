87f3105c8c0fa3b492c99fb98ea1dd6d9613ac8a
Sergey_Begiyan <124567856+serbegiyan@users.noreply.github.com>
2024-12-11 16:22:37 +0300

    lar sh lab translation.php

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/user_number.js'
+                'resources/js/user_number.js',
+                'resources/js/take_number_list.js'
             ],
             refresh: true,
         }),
